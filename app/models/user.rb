c1bef83ff7584b65891a28ab583e52a109557b90
Jason Fu <jasonfu1992@gmail.com>
2019-02-26 08:58:28 -0800

    Add Users and Session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ApplicationRecord
+
+  after_initialize :ensure_session_token
+
+  validates :username, :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: {minimum: 8, allow_nil: true}
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.new(username: username)
+
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
 end

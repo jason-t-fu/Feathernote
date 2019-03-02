ed7551140266d1489525eafe57baad5a7dea5b7e
Jason Fu <jasonfu1992@gmail.com>
2019-03-01 16:47:00 -0800

    Added required modules in webpack and package.json for quill

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,32 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
   },
+  resolve: {
+    alias: {
+      'quill$': path.resolve(__dirname, 'node_modules/quill/quill.js')
+    },
+    extensions: ['.js', 'svg']
+  },
   module: {
     rules: [
       {
         test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
+        exclude: /node_modules(!?\/quill)/,
         use: {
           loader: 'babel-loader',
           query: {
             presets: ['@babel/env', '@babel/react']
           }
         },
+      },
+      {
+        test: /\.svg$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }]
       }
     ]
   },

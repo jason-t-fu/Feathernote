c608b2439e585cd2cc299bd8cebd82fd98b5b9ce
Jason Fu <jasonfu1992@gmail.com>
2019-03-07 17:20:59 -0800

    Add notebook search bar

--- frontend/components/notebooks/notebooks_index.jsx
+++ frontend/components/notebooks/notebooks_index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NotebooksIndexItemContainer from './notebooks_index_item_container';
 
 const NotebookIndex = props => {
+  const [search, setSearch] = useState("");
 
   const openCreateModal = () => {
     props.closeModal();
@@ -11,15 +12,29 @@ const NotebookIndex = props => {
   return (
     <aside className="notebooks-index">
       <div className="notebooks-index-header">
-        <h3 className="notebooks-title">Notebooks</h3>
-        <i className="fas fa-book-medical" onClick={openCreateModal}></i>
+        <div className="notebooks-header-row">
+          <h3 className="notebooks-title">Notebooks</h3>
+          <i className="fas fa-book-medical" onClick={openCreateModal}></i>
+        </div>
+        <form>
+          <input className="notebooks-search-bar"
+                 type="text"
+                 placeholder="Find a notebook"
+                 value={search}
+                 onChange={(e) => setSearch(e.currentTarget.value)} />
+        </form>
       </div>
 
+
       <ul className="notebooks-index-items">
-        {props.notebooks.map(notebook => {
+        {props.notebooks.filter( notebook => {
+          return notebook.title.toLocaleLowerCase()
+                 .startsWith(search.toLocaleLowerCase());
+        }
+        ).map(notebook => {
           return <NotebooksIndexItemContainer key={notebook.id}
                     notebook={notebook}
-                    />
+                    />;
         })}
       </ul>
     </aside>
